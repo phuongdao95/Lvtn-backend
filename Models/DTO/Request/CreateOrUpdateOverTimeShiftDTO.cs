2207804f08cdbfef967e5f494d9ae2c0735aea5b
Minh Nhat <nhat.tangminh71@hcmut.edu.vn>
2022-12-13 00:09:43 +0700

    Resolve conflict

---
+++ Models/DTO/Request/CreateOrUpdateOverTimeShiftDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Models.DTO.Request
+{
+    public class CreateOrUpdateOverTimeShiftDTO
+    {
+        public string? Name { get; set; }
+        public int GroupId { get; set; }
+        public DateTime RegistrationStartDate { get; set; }
+        public DateTime RegistrationEndDate { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string? FormulaName { get; set; }
+        public string? Description { get; set; }
+    }
+}
